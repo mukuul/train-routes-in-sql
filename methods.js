16f93c4ade9f55f325f60b56946fc53f25a9e4f4
mukuul <mukul654@gmail.com>
2021-03-12 15:25:30 +0530

    add prettier

--- methods.js
+++ methods.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql';
+import mysql from "mysql";
 const port = 3000;
 
 const pool = mysql.createPool({
@@ -6,36 +6,35 @@ const pool = mysql.createPool({
   port: 3306,
   user: "root",
   password: "password",
-  database: "train"
+  database: "train",
 });
-
 
 export const addToTable = async (body, table) => {
   let entry;
   if (table === "station") {
     entry = {
-      "stationName": body["stationName"],
-      "stationCode": body["stationCode"],
+      stationName: body["stationName"],
+      stationCode: body["stationCode"],
     };
-  }
-  else if (table === "train") {
+  } else if (table === "train") {
     entry = {
-      "trainNumber": body["trainNumber"],
-      "train": body["train"]
-    }
-  }
-  else if (table === "trainroute") {
+      trainNumber: body["trainNumber"],
+      train: body["train"],
+    };
+  } else if (table === "trainroute") {
     entry = {
       trainID: body["trainID"],
       stationID: body["stationID"],
       time: body["time"],
-      stay: body["stay"]
-    }
+      stay: body["stay"],
+    };
   }
   return new Promise((resolve, reject) => {
     pool.getConnection((err, conn) => {
       if (err) throw err;
-      conn.query(`INSERT INTO ${table} SET ?`, entry,
+      conn.query(
+        `INSERT INTO ${table} SET ?`,
+        entry,
         (error, results, fields) => {
           conn.release();
           if (error) reject(error);
@@ -43,128 +42,131 @@ export const addToTable = async (body, table) => {
             console.log({ id: results.insertId, ...entry });
             resolve({ id: results.insertId, ...entry });
           }
-        });
+        }
+      );
     });
   });
-}
+};
 
 export const search = async (id, table) => {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, conn) => {
       if (err) throw err;
-      conn.query(`SELECT * FROM ${table} WHERE id=?;`, id,
+      conn.query(
+        `SELECT * FROM ${table} WHERE id=?;`,
+        id,
         (error, results, fields) => {
           conn.release();
-          console.log(results)
+          console.log(results);
           if (error) reject(error);
           else if (results.affectedRows === 0) {
-            resolve({ error: "not_found" })
-          }
-          else {
-            resolve(results)
+            resolve({ error: "not_found" });
+          } else {
+            resolve(results);
           }
         }
-      )
-    })
-  })
-}
+      );
+    });
+  });
+};
 export const searchAll = async (table) => {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, conn) => {
       if (err) throw err;
-      conn.query(`SELECT * FROM ${table};`,
-        (error, results, fields) => {
-          conn.release();
-          if (error) reject(error);
-          else {
-            resolve(results)
-          }
+      conn.query(`SELECT * FROM ${table};`, (error, results, fields) => {
+        conn.release();
+        if (error) reject(error);
+        else {
+          resolve(results);
         }
-      )
-    })
-  })
-}
+      });
+    });
+  });
+};
 export const update = (body, id, table) => {
   let entry;
   if (table === "station") {
     entry = {
-      "stationName": body["stationName"],
-      "stationCode": body["stationCode"],
+      stationName: body["stationName"],
+      stationCode: body["stationCode"],
     };
-  }
-  else if (table === "train") {
+  } else if (table === "train") {
     entry = {
-      "trainNumber": body["trainNumber"],
-      "train": body["train"]
-    }
+      trainNumber: body["trainNumber"],
+      train: body["train"],
+    };
   }
   return new Promise((resolve, reject) => {
     pool.getConnection((err, conn) => {
       if (err) throw err;
-      conn.query(`UPDATE ${table} SET ? WHERE id=?;`, [entry, id],
+      conn.query(
+        `UPDATE ${table} SET ? WHERE id=?;`,
+        [entry, id],
         (error, results, fields) => {
           conn.release();
           if (error) reject(error);
           else if (results.affectedRows === 0) {
-            resolve({ error: "not_found" })
-          }
-          else {
-            console.log(results)
+            resolve({ error: "not_found" });
+          } else {
+            console.log(results);
             console.log({ id: id, ...body });
             resolve({ id: id, ...body });
           }
-        });
+        }
+      );
     });
   });
-}
+};
 export const remove = async (id, table) => {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, conn) => {
       if (err) throw err;
-      conn.query(`DELETE FROM ${table} WHERE id=?;`, id,
+      conn.query(
+        `DELETE FROM ${table} WHERE id=?;`,
+        id,
         (error, results, fields) => {
           conn.release();
           if (error) reject(error);
           else if (results.affectedRows === 0) {
-            resolve({ error: "not_found" })
-          }
-          else {
-            resolve("Deleted record for id:" + id)
+            resolve({ error: "not_found" });
+          } else {
+            resolve("Deleted record for id:" + id);
           }
         }
-      )
-    })
-  })
-}
+      );
+    });
+  });
+};
 export const removeAll = async (table) => {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, conn) => {
       if (err) throw err;
-      conn.query(`DELETE FROM ${table};`,
-        (error, results, fields) => {
-          conn.release();
-          if (error) reject(error);
-          else {
-            resolve("Deleted all records")
-          }
+      conn.query(`DELETE FROM ${table};`, (error, results, fields) => {
+        conn.release();
+        if (error) reject(error);
+        else {
+          resolve("Deleted all records");
         }
-      )
-    })
-  })
-}
+      });
+    });
+  });
+};
 export const trainroute = async (query) => {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, conn) => {
       if (err) throw err;
-      conn.query(`SELECT A.trainID FROM trainroute A, trainroute B WHERE A.trainID=B.trainID AND A.stationID='${query.from}' AND B.stationID='${query.to}' AND A.time<B.time;`,
+      conn.query(
+        `SELECT A.trainID FROM trainroute A, trainroute B 
+      WHERE A.trainID=B.trainID AND A.stationID='${query.from}' 
+      AND B.stationID='${query.to}' AND A.time<B.time;`,
         (error, results, fields) => {
           conn.release();
           if (error) reject(error);
           else {
-            resolve(results)
+            resolve(results);
           }
         }
-      )
-    })
-  })
-}
+      );
+    });
+  });
+};
